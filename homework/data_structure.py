6b0f96b20ad8a110dc2a966d4a71e878f651bddd
xbizydown <74044803+xbizydown@users.noreply.github.com>
2024-10-16 05:45:40 +0400

    Tkinter Calculator source code, first try

---
+++ homework/data_structure.py
@@ -0,0 +1,31 @@
+def calculate_structure_sum(data):
+    total_sum = 0
+
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(key, str):
+                total_sum += len(key)
+            total_sum += calculate_structure_sum(value)
+    elif isinstance(data, list):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, tuple):
+        for item in data:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(data, (int, float)):
+        total_sum += data
+    elif isinstance(data, str):
+        total_sum += len(data)
+
+    return total_sum
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
