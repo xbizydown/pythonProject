cb6232f8a923c186cf31115c3b2a80a21640927b
xbizydown <74044803+xbizydown@users.noreply.github.com>
2024-10-17 19:09:23 +0400

    Tkinter Calculator source code, first try

--- homework/data_structure.py
+++ homework/data_structure.py
@@ -1,30 +1,66 @@
-def calculate_structure_sum(data):
+# FIRST VERSION
+
+# def calculate_structure_sum(data):
+#     total_sum = 0
+#
+#     if isinstance(data, dict):
+#         for key, value in data.items():
+#             if isinstance(key, str):
+#                 total_sum += len(key)
+#             total_sum += calculate_structure_sum(value)
+#     elif isinstance(data, list):
+#         for item in data:
+#             total_sum += calculate_structure_sum(item)
+#     elif isinstance(data, tuple):
+#         for item in data:
+#             total_sum += calculate_structure_sum(item)
+#     elif isinstance(data, (int, float)):
+#         total_sum += data
+#     elif isinstance(data, str):
+#         total_sum += len(data)
+#
+#     return total_sum
+#
+# data_structure = [
+#     [1, 2, 3],
+#     {'a': 4, 'b': 5},
+#     (6, {'cube': 7, 'drum': 8}),
+#     "Hello",
+#     ((), [{(2, 'Urban', ('Urban2', 35))}])
+# ]
+#
+# result = calculate_structure_sum(data_structure)
+# print(result)
+
+# SECOND VERSION
+
+def calculate_structure_sum(*data):
     total_sum = 0
 
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(key, str):
-                total_sum += len(key)
-            total_sum += calculate_structure_sum(value)
-    elif isinstance(data, list):
-        for item in data:
-            total_sum += calculate_structure_sum(item)
-    elif isinstance(data, tuple):
-        for item in data:
-            total_sum += calculate_structure_sum(item)
-    elif isinstance(data, (int, float)):
-        total_sum += data
-    elif isinstance(data, str):
-        total_sum += len(data)
+    for item in data:
+
+        if isinstance(item, dict):
+            for key, value in item.items():
+                total_sum += calculate_structure_sum(key)
+                total_sum += calculate_structure_sum(value)
+
+        elif isinstance(item, (list, tuple, set)):
+            total_sum += calculate_structure_sum(*item)
+
+        elif isinstance(item,(int, float)):
+            total_sum += item
+
+        elif isinstance(item, str):
+            total_sum += len(item)
 
     return total_sum
 
 data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 result = calculate_structure_sum(data_structure)
