8ed09d5eca68dfc7d219b1c6a8d3c65f46c54216
xbizydown <74044803+xbizydown@users.noreply.github.com>
2024-10-23 16:41:56 +0400

    Tkinter Calculator source code, first try

---
+++ package1/main.py
@@ -0,0 +1,7 @@
+from package1.package2.package3.doing_nothing import hello
+from package1.package2.doing_something import good_word
+
+
+if __name__ == '__main__':
+    hello()
+    good_word("Aboba")
---
+++ package1/package2/doing_something.py
@@ -0,0 +1,5 @@
+def good_word(name):
+    print(f"{name} you are the best!")
+
+if __name__ == '__main__':
+    good_word("Aboba")
