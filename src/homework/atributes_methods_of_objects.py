761fdc010c3aceb2c99ea56dbf5de062d12d6ba5
xBizyDown <xbizydown@gmail.com>
2024-11-04 06:18:18 +0400

    Fixing some stuff

--- src/homework/atributes_methods_of_objects.py
+++ src/homework/atributes_methods_of_objects.py
@@ -8,15 +8,18 @@ class House:
         if new_floor < 1 or new_floor > self.floors:
             print("Invalid floor number!")
         else:
-            print(f"You are now on the {new_floor} floor.")
+            for floor in range(1, new_floor + 1):
+                print(f"You are now on the {floor} floor.")
 
     def complex_info(self):
         print(f"This is the {self.complex_name} complex.")
+
+    # def __del__(self):
+    #         print("The house has been destroyed.")
+
 elbrus = House("Elbrus", 30)
 elbrus.complex_info()
-elbrus.go_to(1)
-elbrus.go_to(2)
-elbrus.go_to(3)
-elbrus.go_to(4)
 elbrus.go_to(5)
 elbrus.go_to(31)
+
+#del elbrus
